b64605b05a9c21a7054ad20982f5e90bc17e5a05
Alabi Abayomi <abayomi15@gmail.com>
2021-07-22 07:11:47 +0100

    contact page complete

---
+++ asset/js/main.js
@@ -0,0 +1,7 @@
+// Get access to the nav item container from html
+const navItemContainer = document.getElementById(nav-item);
+
+// function that opens and close navbar menu
+function toggleNavbar() {
+    navItemContainer.classList.toggle("open-nav-item")
+}
