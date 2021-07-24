69281f9bd63f423265edcc2ea718fa4784152e23
Alabi Abayomi <abayomi15@gmail.com>
2021-07-24 15:22:09 +0100

    fixed navbar

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,7 +1,7 @@
 // Get access to the nav item container from html
-const navItemContainer = document.getElementById(nav-item);
+const navItemContainer = document.getElementById("nav-items");
 
 // function that opens and close navbar menu
 function toggleNavbar() {
-    navItemContainer.classList.toggle("open-nav-item")
+  navItemContainer.classList.toggle("open-nav-item");
 }
